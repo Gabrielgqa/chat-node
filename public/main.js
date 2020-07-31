420d4dd27ccad1dd1a8ec9fdc5ad9929aa1f732c
GabrielGQA <gabrielgqa@gmail.com>
2020-07-31 19:58:50 -0300

    Entra na sala

--- public/main.js
+++ public/main.js
@@ -12,12 +12,31 @@ let chatMessage = document.querySelector('#chatMessage');
 loginPage.style.display = 'flex';
 chatPage.style.display = 'none';
 
+function renderUserList() {
+  let ul = document.querySelector('.userList');
+  ul.innerHTML = '';
+
+  userList.forEach(user => {
+    ul.innerHTML += '<li>'+user+'</li>';
+  })
+}
+
 loginName.addEventListener('keyup', (e) => {
   if(e.keyCode === 13){
     let name = loginName.value.trim();
     if(name != ' '){
       username = name;
       document.title = 'Chat ('+username+')';
+      socket.emit('join-request', username);
     }
   }
+});
+
+socket.on('user-ok', (connectedUsers) => {
+  loginPage.style.display = 'none';
+  chatPage.style.display = 'flex';
+  chatMessage.focus();
+
+  userList = connectedUsers;
+  renderUserList();
 })
